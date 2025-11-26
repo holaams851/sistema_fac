bdc36d50b7111a90edfe916e75b1fceb47980ae0
CARLOS <careyhgar.com>
2025-11-26 17:52:46 -0600

    Index modificado

--- index.php
+++ index.php
@@ -33,24 +33,17 @@ while ($row = $res_mes->fetch_assoc()) {
     <link rel="icon" href="../../../../favicon.ico">
 
     <title>Sistema de Facturación: Toner & Más</title>
-    <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-    <!-- Custom styles for this template -->
-    <link href="dashboard.css" rel="stylesheet">
+    <link href="/sistema_fac/dashboard.css?v=3" rel="stylesheet">
 
-    <!-- Chart.js -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js"></script>
   </head>
 
   <body>
-     <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Toner & Más</a>
-    </nav>
-
     <div class="container-fluid">
       <div class="row">
-        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+        <nav class="col-md-2 d-none d-md-block sidebar">
           <div class="sidebar-sticky">
             <ul class="nav flex-column">
               <li class="nav-item">
@@ -60,15 +53,15 @@ while ($row = $res_mes->fetch_assoc()) {
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="ver_todas_facturas.php">
-                  <span data-feather="file"></span>
-                  Facturas
+                <a class="nav-link" href="clientes.php">
+                  <span data-feather="users"></span>
+                  Clientes
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="crud_facturas/crear_factura.php">
-                  <span data-feather="plus-circle"></span>
-                  Crear Factura
+                <a class="nav-link" href="proveedores.php">
+                  <span data-feather="truck"></span>
+                  Proveedores
                 </a>
               </li>
               <li class="nav-item">
@@ -78,9 +71,15 @@ while ($row = $res_mes->fetch_assoc()) {
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link" href="clientes.php">
-                  <span data-feather="users"></span>
-                  Clientes
+                <a class="nav-link" href="crud_facturas/crear_factura.php">
+                  <span data-feather="plus-circle"></span>
+                  Crear Factura
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="ver_todas_facturas.php">
+                  <span data-feather="file"></span>
+                  Facturas
                 </a>
               </li>
               <li class="nav-item">
@@ -99,36 +98,35 @@ while ($row = $res_mes->fetch_assoc()) {
           </div>
         </nav>
 
-        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+        <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
           <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
             <h1 class="h2">Dashboard</h1>
             <div class="btn-toolbar mb-2 mb-md-0">
             </div>
           </div>
 
-        <?php if (empty($meses)) { ?>
-            <p style="color:red; text-align:center;">HOLA ME LLAMO JUAN No hay datos para mostrar. Asegúrate de tener facturas con detalles y totales.</p>
-        <?php } else { ?>
-          <canvas class="my-4" id="ventasMes" width="800" height="300"></canvas>
-        <?php } ?>
+          <?php if (empty($meses)) { ?>
+              <p style="color:red; text-align:center;">No hay datos para mostrar. Asegúrate de tener facturas con detalles y totales.</p>
+          <?php } else { ?>
+            <canvas class="my-4" id="ventasMes" width="800" height="300"></canvas>
+          <?php } ?>
 
           <h2>Facturas Recientes</h2>
-           <?php
+            <?php
             $sql = "SELECT * FROM Detalle_Factura ORDER BY id_detalle DESC"; // DESC = descendente
             $result = $conn->query($sql);
             mostrarTabla($result, ['id_factura','nombre_equipo','cantidad','total']);
-          ?>
+            ?>
           
+        </main>
       </div>
     </div>
     
-    <!-- Icons -->
     <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
     <script>
       feather.replace()
     </script>
 
-    <!-- Graphs -->
     <script>
       const ctx = document.getElementById('ventasMes')?.getContext('2d');
         if (ctx) {
@@ -158,7 +156,7 @@ while ($row = $res_mes->fetch_assoc()) {
                     display: false,
                 }
             }
-         })};
+          })};
     </script>
   </body>
 </html>
