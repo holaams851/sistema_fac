13b3fb19398e528ac381a565040935ad647ef6da
Andrea Aragon <andreaaragon851@gmail.com>
2025-11-27 03:15:47 -0600

    Add labor cost to invoice detail insertion

--- guardar_factura.php
+++ guardar_factura.php
@@ -42,13 +42,6 @@ if (!$conn->query($sql_factura)) {
     exit;
 }   
 
-$sql_mano_obra = "INSERT INTO Detalle_Factura (mano_de_obra) VALUES ('$mano_de_obra')";
-if (!$conn->query($sql_mano_obra)) {
-    // Si falla la inserción, redirigimos con error
-    header("Location: crud_facturas/crear_factura.php?error=db_factura");
-    exit;
-}
-
 $id_factura = $conn->insert_id;
 
 // 5. Insertar Detalle de Factura
@@ -71,8 +64,8 @@ if (!empty($_POST['equipos']['id'])) {
         $total_final += $subtotal;
 
         if ($id_equipo > 0) {
-            $sql_detalle = "INSERT INTO Detalle_Factura (id_factura, cantidad, precio_unitario, id_equipo, nombre_equipo, total, subtotal)
-                             VALUES ('$id_factura', '$cantidad', '$precio', '$id_equipo', '$nombre', '$total_final', '$subtotal')";
+            $sql_detalle = "INSERT INTO Detalle_Factura (id_factura, cantidad, precio_unitario, id_equipo, nombre_equipo, total, subtotal, mano_de_obra)
+                             VALUES ('$id_factura', '$cantidad', '$precio', '$id_equipo', '$nombre', '$total_final', '$subtotal', '$mano_de_obra')";
             $conn->query($sql_detalle);
         }
     }
