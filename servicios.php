277022dcfee159f72ad9446d2882b9874cae8507
Andrea Aragon <andreaaragon851@gmail.com>
2025-11-27 01:24:06 -0600

    Resolviendo error de cache CSS

--- servicios.php
+++ servicios.php
@@ -8,7 +8,8 @@ $result = $conn->query("SELECT * FROM Servicio");
 <head>
   <meta charset="UTF-8">
   <title>Agregar Servicio</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link href="dashboard.css?v=7" rel="stylesheet">
 </head>
 <body>
     <div class="container mt-5">
